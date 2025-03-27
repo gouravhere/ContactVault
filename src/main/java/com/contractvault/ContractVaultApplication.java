1ec56ccb0c9f053e3412a3429a313c7fe0c6f422
gourav sharma <gourav.101sharma@gmail.com>
2025-03-27 22:20:54 +0530

    latest

--- src/main/java/com/contractvault/ContractVaultApplication.java
+++ src/main/java/com/contractvault/ContractVaultApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//main controller new
+//main controller new --
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.contractvault",
 	    "com.contractvault.services",
